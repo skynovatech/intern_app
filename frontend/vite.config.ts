e78e7c78917f4be322138fd6b40ec9551903f849
Nawfal Aadil S <nawfalaadil@gmail.com>
2026-07-25 19:30:30 +0530

    updated the API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
-      "/uploads": "http://localhost:8000",
+      "/api": "https://intern-app-lxil.onrender.com",
+      "/uploads": "https://intern-app-lxil.onrender.com",
     },
   },
 });
